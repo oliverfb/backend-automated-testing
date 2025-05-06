def6b9d80fc8cffab1fafeeaf6e8b0e53b724f2a
oliverfb <oliver-felix@hotmail.com>
2025-05-05 23:22:16 -0400

    add logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { setupRoutes } = require('./routes/index');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const loggerFormat = process.env.NODE_ENV === 'development' ? 'dev' : 'combined';
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
@@ -14,6 +16,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
+
+// Logger
+app.use(morgan(loggerFormat));
 
 // Basic route
 app.get('/', (req, res) => {
